cac1394c2e9541e4ec10bac079403555e8df1d4c
sooni-roh <127611478+sooni-roh@users.noreply.github.com>
2023-08-31 10:32:51 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,60 @@
-# 환경 변수
-# from dotenv import load_dotenv
-# load_dotenv()
-
-
-# 스트림릿
-import streamlit as st
-# ChatOpenAI
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
+import streamlit as st
+import tempfile
+import os
 
+# 제목
+st.title("ChatPDF")
+st.write("---")
 
-# 인공지능에게 시를 요청하기
-chat_model = ChatOpenAI()
-# content = "호주 여행" 
-# result = chat_model.predict(content + "에 대한 시를 써줘")
-# print(result)
+# 파일 업로드
+uploaded_file = st.file_uploader("Choose a file")
+st.write("---")
 
+# 업로드된 PDF 파일을 임시 디렉토리에 저장하고, 
+# 해당 파일을 로드하여 페이지를 분할한 후
+# 각 페이지를 반환하는 기능을 수행
+def pdf_to_document(uploaded_file):
+    temp_dir = tempfile.TemporaryDirectory()
+    temp_filepath = os.path.join(temp_dir.name, uploaded_file.name)
+    with open(temp_filepath, "wb") as f:
+        f.write(uploaded_file.getvalue())
+    loader = PyPDFLoader(temp_filepath)
+    pages = loader.load_and_split()
+    return pages
 
-# 인공지능 시인 화면 구성하기
-st.title('인공지능 시인')
-content = st.text_input('시의 주제를 제시해주세요.', '')
+# 업로드 되면 동작하는 코드
+if uploaded_file is not None:
+    pages = pdf_to_document(uploaded_file)
 
-if st.button('시 작성 요청하기'):
-    with st.spinner('시 작성 중...'):
-        result = chat_model.predict("write a poet" + content + "에 대한 시를 써줘")
-        st.write(result)
+    # Split
+    text_splitter = RecursiveCharacterTextSplitter(
+        # Set a really small chunk size, just to show.
+        chunk_size = 300,
+        chunk_overlap  = 20,
+        length_function = len,
+        is_separator_regex = False,
+    )
+    texts = text_splitter.split_documents(pages)
+
+    # Embedding
+    embeddings_model = OpenAIEmbeddings()
+
+    # load it into Chroma
+    db = Chroma.from_documents(texts, embeddings_model)
+
+    # Question
+    st.header("PDF에게 질문해보세요!!")
+    question = st.text_input('질문을 입력하세요')
+
+    if st.button('질문하기'):
+        with st.spinner('Wait for it...'):
+            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+            qa_chain = RetrievalQA.from_chain_type(llm,retriever=db.as_retriever())
+            result = qa_chain({"query": question})
+            st.write(result["result"])
